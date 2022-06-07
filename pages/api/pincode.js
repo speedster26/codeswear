38b4ee323e4b7bd9b58457c4504d5d8260187208
Harshit Verma <harshitverma591@gmail.com>
2022-06-07 16:01:54 +0530

    Second Commit

---
+++ pages/api/pincode.js
@@ -0,0 +1,6 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+    res.status(200).json([282007, 282009, 111005, 110048, 568874])
+  }
+  
